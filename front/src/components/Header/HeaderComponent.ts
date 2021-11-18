9063c1b8e3c48ecff76b48eff9098de646f03fa0
Yeseo Lee <6h15m.develop@gmail.com>
2021-11-18 16:15:59 +0900

    feat(front): 장바구니 기본 정보 #12

--- front/src/components/Header/HeaderComponent.ts
+++ front/src/components/Header/HeaderComponent.ts
@@ -9,7 +9,7 @@ export class HeaderComponent extends HTMLElement {
       <header>
         <div class="left-container">
             <h1><a href="http://localhost:8081/">Untitled.</a></h1>
-            <input class="search-container" type="text" placeholder="search"/>
+            <input class="search-container" type="text" placeholder="Search"/>
         </div>
         <div class="right-container">
             <a href="${"/cart"}">Cart</a>
---
+++ front/src/sample-data/detailed_products_option_properties.ts
@@ -0,0 +1,7 @@
+export const DETAILED_PRODUCTS_OPTION_PROPERTIES = [
+  { detailed_product_id: 1, option_property_id: 2 },
+  { detailed_product_id: 1, option_property_id: 5 },
+  { detailed_product_id: 2, option_property_id: 3 },
+  { detailed_product_id: 2, option_property_id: 6 },
+  { detailed_product_id: 3, option_property_id: 1 },
+];
