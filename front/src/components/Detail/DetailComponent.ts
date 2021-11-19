3b71b920b47410b2e8fe9e7991eb249f76a38c1e
Yeseo Lee <6h15m.develop@gmail.com>
2021-11-19 18:56:37 +0900

    feat(front): 상세 페이지 수정 #34

--- front/src/components/Detail/DetailComponent.ts
+++ front/src/components/Detail/DetailComponent.ts
@@ -21,47 +21,54 @@ export class DetailComponent extends HTMLElement {
     super();
     const p = PRODUCTS[product_id - 1];
     const detailContent = `
-        <div class="wrap">
-            <div class="product-image"></div>
-            <div class="product-info">
-                <div class="product-category">
-                    ${getBigCategoryNameBySmallId(p.small_category_id)} > ${getSmallCategoryNameById(
+      <div class="wrap">
+        <div class="product-image"></div>
+        <div class="product-info">
+          <div class="product-category">
+            ${getBigCategoryNameBySmallId(p.small_category_id)} > ${getSmallCategoryNameById(
       p.small_category_id,
     )}
+          </div>
+          <h2 class="product-name">${p.product_name}</h2>
+          <div class="product-tags">
+            ${
+              join(
+                map(
+                  (t_id) => `<div class="tag-name"># ${getTagNameById(t_id)}</div>`,
+                  getTagIdByProductId(p.product_id),
+                ),
+              ) ?? ''
+            }
+          </div>
+          <div class="product-options">
+            ${join(
+              map(
+                (o) => `
+                <h4 class="option-name">${getOptionNameByOptionId(o.option_id)}</h4>
+                <div class="option-property-container">
+                  ${join(
+                    map(
+                      (op) => `
+                      <div class="option-property">
+                        <input type="radio" value=${op.option_property_id} class="option-property-name"> ${op.option_property_name}</input>
+                      </div>
+                      `,
+                      getOptionPropertyByOptionId(o.option_id),
+                    ),
+                  )}
                 </div>
-                <h2 class="product-name">${p.product_name}</h2>
-                <div class="product-tags">
-                    ${join(
-                      map(
-                        (t_id) => `<div class="tag-name"># ${getTagNameById(t_id)}</div>`,
-                        getTagIdByProductId(p.product_id),
-                      ),
-                    )}
-                </div>
-                <div class="product-options">
-                ${join(
-                  map(
-                    (o) => `
-                        <h4 class="option-name">${getOptionNameByOptionId(o.option_id)}</h4>
-                        <div class="option-property-name-container">
-                            ${join(
-                              map(
-                                (op) =>
-                                  `<input type="radio" value=${op.option_property_id} class="option-property-name"> ${op.option_property_name}</input>`,
-                                getOptionPropertyByOptionId(o.option_id),
-                              ),
-                            )}</div>
-                        `,
-                    getOptionIdByProductId(p.product_id),
-                  ),
-                )}
-                </div>
-                <div class="product-price">
-                  ${p.product_price}
-                </div>
-            </div>
+              `,
+                getOptionIdByProductId(p.product_id),
+              ),
+            )}
+          </div>
+          <div class="product-price">
+            ${p.product_price.toLocaleString('ko-KR')}
+          </div>
         </div>
-        `;
+      </div>
+    `;
+
     const detailStyle = document.createElement('style');
     detailStyle.textContent = styl;
     const shadowRoot = this.attachShadow({ mode: 'open' });
