979d15ebb71d016b3a6dafcb5699024d44128952
Yeseo Lee <6h15m.develop@gmail.com>
2021-11-24 18:38:00 +0900

    feat(front, back): `CategoryComponent`와 DB 연결

--- front/src/models/product.interface.ts
+++ front/src/models/product.interface.ts
@@ -1,10 +1,10 @@
 export interface ProductType {
-  product_id: number,
-  product_name: string,
-  product_price: number,
-  small_category_id: number
+  product_id: number;
+  product_name: string;
+  product_price: number;
+  small_category_id: number;
 }
 
 export interface ProductsType {
-  rows: Array<ProductType>
+  rows: Array<ProductType>;
 }
