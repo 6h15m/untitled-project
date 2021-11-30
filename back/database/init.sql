b33db8ba5e79223bbe38edf92c5f4cec5865f2e9
Yeseo Lee <6h15m.develop@gmail.com>
2021-11-30 15:11:11 +0900

    feat(back): AUTO INCREMENT 설정

--- back/database/init.sql
+++ back/database/init.sql
@@ -13,19 +13,19 @@ DROP TABLE products_tags CASCADE;
 DROP TABLE products_options CASCADE;
 
 CREATE TABLE big_categories (
-    big_category_id INT PRIMARY KEY,
+    big_category_id SERIAL PRIMARY KEY,
     big_category_name VARCHAR
 );
 
 CREATE TABLE small_categories (
-    small_category_id INT PRIMARY KEY,
+    small_category_id SERIAL PRIMARY KEY,
     small_category_name VARCHAR,
     big_category_id INT,
     FOREIGN KEY (big_category_id) REFERENCES big_categories (big_category_id)
 );
 
 CREATE TABLE products (
-    product_id INT PRIMARY KEY,
+    product_id SERIAL PRIMARY KEY,
     product_name VARCHAR,
     product_price INT,
     small_category_id INT,
@@ -39,17 +39,17 @@ CREATE TABLE users (
 );
 
 CREATE TABLE tags (
-    tag_id INT PRIMARY KEY,
+    tag_id SERIAL PRIMARY KEY,
     tag_name VARCHAR
 );
 
 CREATE TABLE options (
-    option_id INT PRIMARY KEY,
+    option_id SERIAL PRIMARY KEY,
     option_name VARCHAR
 );
 
 CREATE TABLE option_properties (
-    option_property_id INT PRIMARY KEY,
+    option_property_id SERIAL PRIMARY KEY,
     option_property_name VARCHAR,
     option_property_additional_price INT,
     option_property_base BOOLEAN,
@@ -74,7 +74,7 @@ CREATE TABLE products_options (
 
 -- 장바구니 관련 테이블
 CREATE TABLE detailed_products (
-    detailed_product_id INT PRIMARY KEY,
+    detailed_product_id SERIAL PRIMARY KEY,
     product_id INT,
     FOREIGN KEY (product_id) REFERENCES products (product_id)
 );
@@ -95,67 +95,67 @@ CREATE TABLE carts (
 );
 
 -- 샘플 데이터
-INSERT INTO big_categories VALUES
-    (1, 'Apparel'),
-    (2, 'Accessories'),
-    (3, 'Stationary');
+INSERT INTO big_categories (big_category_name) VALUES
+    ('Apparel'),
+    ('Accessories'),
+    ('Stationary');
 
-INSERT INTO small_categories VALUES
-    (1, 'Hoodies', 1),
-    (2, 'T-shirts', 1),
-    (3, 'Pants', 1),
-    (4, 'Pouches', 2),
-    (5, 'Bags', 2),
-    (6, 'Keychains', 2),
-    (7, 'Jewelry', 2),
-    (8, 'Tin Cases', 3),
-    (9, 'Mouse Pads', 3),
-    (10, 'Pens', 3);
+INSERT INTO small_categories (small_category_name, big_category_id) VALUES
+    ('Hoodies', 1),
+    ('T-shirts', 1),
+    ('Pants', 1),
+    ('Pouches', 2),
+    ('Bags', 2),
+    ('Keychains', 2),
+    ('Jewelry', 2),
+    ('Tin Cases', 3),
+    ('Mouse Pads', 3),
+    ('Pens', 3);
 
-INSERT INTO products VALUES
-    (1, 'Overweight Hoodie', 25000, 1),
-    (2, 'Crop Hoodie', 20000, 1),
-    (3, 'Crop T-shirt', 12000, 2),
-    (4, 'Character T-shirt', 13000, 2),
-    (5, 'Half Pants', 17000, 3),
-    (6, 'Wide-fit Pants', 18000, 3),
-    (7, 'Cross Bag', 30000, 5),
-    (8, 'Tote Bag', 32000, 5),
-    (9, 'Eco Bag', 15000, 5),
-    (10, 'Necklace', 18500, 7),
-    (11, 'Steel Ring', 16000, 7),
-    (12, 'Square Tin Case', 10000, 8),
-    (13, 'Circle Tin Case', 11000, 8),
-    (14, 'Long Mouse Pad', 16000, 9),
-    (15, 'Short Mouse Pad', 14000, 9),
-    (16, 'Clip Pen', 8000, 10);
+INSERT INTO products (product_name, product_price, small_category_id) VALUES
+    ('Overweight Hoodie', 25000, 1),
+    ('Crop Hoodie', 20000, 1),
+    ('Crop T-shirt', 12000, 2),
+    ('Character T-shirt', 13000, 2),
+    ('Half Pants', 17000, 3),
+    ('Wide-fit Pants', 18000, 3),
+    ('Cross Bag', 30000, 5),
+    ('Tote Bag', 32000, 5),
+    ('Eco Bag', 15000, 5),
+    ('Necklace', 18500, 7),
+    ('Steel Ring', 16000, 7),
+    ('Square Tin Case', 10000, 8),
+    ('Circle Tin Case', 11000, 8),
+    ('Long Mouse Pad', 16000, 9),
+    ('Short Mouse Pad', 14000, 9),
+    ('Clip Pen', 8000, 10);
 
 INSERT INTO users VALUES
     ('test_a', 'test_a', 'admin'),
     ('test_c', 'test_c', 'customer');
 
-INSERT INTO tags VALUES
-    (1, 'Trendy'),
-    (2, 'Modern'),
-    (3, 'Useful'),
-    (4, 'Summer'),
-    (5, 'Deskterior');
+INSERT INTO tags (tag_name) VALUES
+    ('Trendy'),
+    ('Modern'),
+    ('Useful'),
+    ('Summer'),
+    ('Deskterior');
 
-INSERT INTO options VALUES
-    (1, 'Color'),
-    (2, 'Size');
+INSERT INTO options (option_name) VALUES
+    ('Color'),
+    ('Size');
 
-INSERT INTO option_properties VALUES
-    (1, 'Red', 0, true, 1),
-    (2, 'Black', 0, false, 1),
-    (3, 'Blue', 0, false, 1),
-    (4, 'White', 0, false, 1),
-    (5, 'S', 0, true, 2),
-    (6, 'M', 2000, false, 2),
-    (7, 'L', 5000, false, 2),
-    (8, 'XL', 7000, false, 2);
+INSERT INTO option_properties (option_property_name, option_property_additional_price, option_property_base, option_id)VALUES
+    ('Red', 0, true, 1),
+    ('Black', 0, false, 1),
+    ('Blue', 0, false, 1),
+    ('White', 0, false, 1),
+    ('S', 0, true, 2),
+    ('M', 2000, false, 2),
+    ('L', 5000, false, 2),
+    ('XL', 7000, false, 2);
 
-INSERT INTO products_tags VALUES
+INSERT INTO products_tags (product_id, tag_id) VALUES
     (1, 1),
     (3, 1),
     (3, 4),
@@ -165,7 +165,7 @@ INSERT INTO products_tags VALUES
     (15, 5),
     (16, 3);
 
-INSERT INTO products_options VALUES
+INSERT INTO products_options (product_id, option_id) VALUES
     (1, 1),
     (1, 2),
     (2, 1),
@@ -185,19 +185,19 @@ INSERT INTO products_options VALUES
     (15, 1),
     (16, 1);
 
-INSERT INTO detailed_products VALUES
+INSERT INTO detailed_products (detailed_product_id, product_id) VALUES
     (1, 2),
     (2, 2),
     (3, 16);
 
-INSERT INTO detailed_products_option_properties VALUES
+INSERT INTO detailed_products_option_properties (detailed_product_id, option_property_id) VALUES
     (1, 2),
     (1, 5),
     (2, 3),
     (2, 6),
     (3, 1);
 
-INSERT INTO carts VALUES
+INSERT INTO carts (user_id, detailed_product_id, cart_product_amount) VALUES
     ('test_c', 1, 2),
     ('test_c', 2, 1),
     ('test_c', 3, 2);
